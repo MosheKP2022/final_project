ca859eaca62b2c6ea78e7abc098c4367e5c9c4ef
MosheKP2022 <mperry7@chicagobooth.edu>
2022-03-09 21:17:12 +0000

    Labeled Offerings model updated

--- app/models/labeled_offering.rb
+++ app/models/labeled_offering.rb
@@ -9,4 +9,11 @@
 #  tag_id      :integer
 #
 class LabeledOffering < ApplicationRecord
+
+  belongs_to(:offering, { :required => true, :class_name => "Offering", :foreign_key => "offering_id", :counter_cache => true })
+  belongs_to(:tag, { :required => true, :class_name => "Tag", :foreign_key => "tag_id", :counter_cache => true })
+
+  validates(:tag_id, { :presence => true })
+  validates(:offering_id, { :presence => true })
+  
 end
